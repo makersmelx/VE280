93ccc4b180d91fc29ed851acf5008b0b6bb43ca7
makersmelx <makersmelx@gmail.com>
2019-06-04 02:37:22 +0800

    AC version

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -10,17 +10,19 @@ int size(list_t list)
 
 bool memberOf(list_t list, int val)
 {
-	return !list_isEmpty(list) && ((list_first(list) == val) || memberOf(list_rest(list), val));
+	
+	return list_isEmpty(list)?false: ((list_first(list) == val) || memberOf(list_rest(list), val));
 }
 
 int dot(list_t v1, list_t v2)
 {
-	return (list_isEmpty(v1) && list_isEmpty(v2))?0 : (list_first(v1) * list_first(v2) + dot(list_rest(v1), list_rest(v2)));
+	
+	return (list_isEmpty(v1) || list_isEmpty(v2))?0 : (list_first(v1) * list_first(v2) + dot(list_rest(v1), list_rest(v2)));
 }
 
 bool isIncreasing(list_t v)
 {
-	return size(v) <= 1 || (list_first(v) < list_first(list_rest(v)) && isIncreasing(list_rest(v)));
+	return size(v) <= 1 || (list_first(v) <= list_first(list_rest(v)) && isIncreasing(list_rest(v)));
 }
 
 static list_t reverse_helper(list_t list, list_t res)
@@ -50,29 +52,20 @@ static int arith_helper(list_t v)//ensure that has at least two elems
 
 bool isArithmeticSequence(list_t v)
 {
+	
 	return size(v) <= 2 || (arith_helper(v) == arith_helper(list_rest(v)) && isArithmeticSequence(list_rest(v)));
 }
 
-static list_t filter_odd_helper(list_t list, list_t res)
-{
-	return (list_isEmpty(list)) ? res : filter_odd_helper(list_rest(list), ((list_first(list) % 2 == 1) ? list_make(list_first(list), res) : res));
-}
 
 
 list_t filter_odd(list_t list)
 {
-	return filter_odd_helper(list, list_make());
-}
-
-
-static list_t filter_helper(list_t list, list_t res, bool(*fn)(int))
-{
-	return (list_isEmpty(list)) ? res : filter_helper(list_rest(list),( fn(list_first(list)) ? list_make(list_first(list), res) : res) , fn);
+	return list_isEmpty(list)?list_make() : (list_first(list) % 2 == 1) ? list_make(list_first(list),filter_odd(list_rest(list))):filter_odd(list_rest(list));
 }
 
 list_t filter(list_t list, bool(*fn)(int))
 {
-	return filter_helper(list, list_make(),fn);
+	return list_isEmpty(list)?list_make() : fn(list_first(list)) ? list_make(list_first(list),filter(list_rest(list),fn)):filter(list_rest(list),fn);
 }
 
 static list_t unique_helper(list_t list, list_t res)
@@ -82,34 +75,18 @@ static list_t unique_helper(list_t list, list_t res)
 
 list_t unique(list_t list)
 {
-	return unique_helper(list, list_make());
+	return reverse(unique_helper(list, list_make()));
 }
 
-static list_t first_front(list_t first , list_t res,unsigned int n)
-{
-	return (n == 0) ? res : first_front(list_rest(first), list_make(list_first(first), res), n - 1);
-}
-
-static list_t get_back(list_t first, unsigned int n)
-{
-	return (n == 1) ? list_rest(first) : get_back(list_rest(first), n - 1);
-}
 
 list_t insert_list(list_t first, list_t second, unsigned int n)
 {
-	if (n < 0 || n > size(first))
-		return list_make();
-	return append(first_front(first, list_make(), n), append(second, get_back(first, n)));
-}
-
-static list_t chop_helper(list_t first, list_t res, unsigned int n)
-{
-	return (n == 0) ? res : chop_helper(list_rest(first), list_make(list_first(first), res), n - 1);
+	return append(chop(first, size(first) - n), append(second, chop(first, n)));
 }
 
 list_t chop(list_t list, unsigned int n)
 {
-	return (n > 0) ? chop_helper(list, list_make(), size(list) - n) : list;
+	return  n <= 0 ? list :reverse(list_rest(reverse(chop(list,n-1))));
 }
 
 //************************************************************
@@ -122,25 +99,15 @@ static int maxium(int a, int b)
 	return a > b ? a : b;
 }
 
-static int minium(int a, int b)
-{
-	return a < b ? a : b;
-}
+// static int minium(int a, int b)
+// {
+// 	return a < b ? a : b;
+// }
 
-static int tree_sum_helper(tree_t tree, int sum)
-{
-	if (tree_isEmpty(tree))
-		return sum;
-	else
-	{
-		sum += tree_sum_helper(tree_left(tree), sum) + tree_elt(tree) + tree_sum_helper(tree_right(tree), sum);
-		return sum;
-	}
-}
 
 int tree_sum(tree_t tree)
 {
-	return (tree_isEmpty(tree)) ? 0 : tree_sum_helper(tree, 0);
+	return (tree_isEmpty(tree)) ? 0 : (tree_sum(tree_left(tree)) + tree_elt(tree) + tree_sum(tree_right(tree)));
 }
 
 bool tree_search(tree_t tree, int key)
@@ -160,92 +127,84 @@ static int max_helper(tree_t tree, int max)
 
 int tree_max(tree_t tree)
 {	
-	return max_helper(tree, tree_elt(tree));
-}
-
-static void traversal_helper(tree_t tree, list_t &res) // make to list_t return
-{
-	if (tree_isEmpty(tree))
-		return;
-	else
-	{
-		traversal_helper(tree_right(tree), res);
-		res = list_make(tree_elt(tree), res);
-		traversal_helper(tree_left(tree), res);
-	}
+	return tree_isEmpty(tree)? INT32_MIN: max_helper(tree, tree_elt(tree));
 }
 
 list_t traversal(tree_t tree)
 {
-	list_t res = list_make();
-	traversal_helper(tree, res);
-	return res;
+	return tree_isEmpty(tree) ? list_make() : append(traversal(tree_left(tree)), list_make(tree_elt(tree), traversal(tree_right(tree))));
 }
 
-static bool mono_helper(tree_t tree, int mode,int cur,int depth)//1 for increase , 0 for decrease
+static bool mono_helper(tree_t tree, bool(*fn)(int,int))
 {
-	if(tree_isEmpty(tree))
-		return (depth == cur) ? true : false;
-	int mode_l = mode, mode_r = mode;
-	int l = (!tree_isEmpty(tree_left(tree)) && tree_elt(tree_left(tree)) > tree_elt(tree));
-	int r = (!tree_isEmpty(tree_left(tree)) && tree_elt(tree_left(tree)) > tree_elt(tree));
-	if(mode == -1)
-	{
-		mode_l = l;
-		mode_r = r;
-	}
-	return ((mode == l) && mono_helper(tree_left(tree), mode_l, cur + 1, depth)) || ((mode == r) && mono_helper(tree_right(tree), mode_r, cur + 1, depth));
+	return (tree_isEmpty(tree_left(tree)) || (fn(tree_elt(tree), tree_elt(tree_left(tree))) && mono_helper(tree_left(tree), fn))) || (tree_isEmpty(tree_right(tree)) || (fn(tree_elt(tree), tree_elt(tree_right(tree))) && mono_helper(tree_right(tree), fn)));
 }
 
 bool tree_hasMonotonicPath(tree_t tree)
 {
-	return tree_isEmpty(tree) ? false : mono_helper(tree, -1 ,0,depth(tree));
+	return tree_isEmpty(tree) ? false : mono_helper(tree, [](int a, int b)->bool { return a >= b; }) || mono_helper(tree, [](int a, int b)-> bool { return a <= b; });
 }
 
 static bool tree_allPathSumGreater_helper(tree_t tree, int sum, int path)
 {
+	
 	if (tree_isEmpty(tree))
-		return (sum > path) ? true : false;
+		return (path > sum) ? true : false;
 	else
 		return tree_allPathSumGreater_helper(tree_left(tree), sum, path + tree_elt(tree)) && tree_allPathSumGreater_helper(tree_right(tree), sum, path + tree_elt(tree));
 }
 
-
 bool tree_allPathSumGreater(tree_t tree, int sum)
 {
+	
 	return (tree_isEmpty(tree)) ? false : tree_allPathSumGreater_helper(tree, sum, 0);
 }
 		
 bool covered_by(tree_t A, tree_t B)
 {
 	if (tree_isEmpty(A))
-		return !tree_isEmpty(B);
+		return true;
 	else if (tree_isEmpty(B))
 		return false;
 	return tree_elt(A) == tree_elt(B) && covered_by(tree_left(A), tree_left(B)) && covered_by(tree_right(A), tree_right(B));
 }
 
-static bool subtree(tree_t A, tree_t B,bool flag)
+static bool tree_equal(tree_t t1, tree_t t2)
+    // EFFECTS: returns true iff t1 == t2
+{
+    if(tree_isEmpty(t1) && tree_isEmpty(t2))
+    {
+        return true;
+    }
+    else if(tree_isEmpty(t1) || tree_isEmpty(t2))
+    {
+        return false;
+    }
+    else
+    {
+        return ((tree_elt(t1) == tree_elt(t2)) 
+            && tree_equal(tree_left(t1), tree_left(t2))
+            && tree_equal(tree_right(t1), tree_right(t2)));
+    }
+}
+
+static bool subtree(tree_t A, tree_t B)
 {
 	if (tree_isEmpty(B))
-		return false;
-	if (flag)
-		return tree_elt(A) == tree_elt(B) && subtree(tree_left(A), tree_left(B), flag) && subtree(tree_right(A), tree_right(B), flag);
-	else
-	{
-		if (tree_elt(A) == tree_elt(B))
-			flag = true;
-		return subtree(A, tree_left(B), flag) || subtree(A, tree_right(B), flag);
-	}	
+		return tree_isEmpty(A)?true:false;
+	if (tree_elt(A) == tree_elt(B))
+		return tree_equal(A,B);
+	return subtree(A, tree_left(B)) || subtree(A, tree_right(B));
 }
 
 bool contained_by(tree_t A, tree_t B)
 {
-	return (covered_by(A, B)) || subtree(A, B,false);
+	return (covered_by(A, B)) || subtree(A, B);
 }
 
 tree_t insert_tree(int elt, tree_t tree)
 {
+
 	if (tree_isEmpty(tree))
 		return tree_make(elt, tree_make(), tree_make());
 	else
