df5d868b184566be0cafdd7c76cf1291aa04aa6a
makersmelx <makersmelx@gmail.com>
2019-07-14 12:55:22 +0800

    批量比较SHELL和一个平局实例

---
+++ p4/sh_test/app.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# usage ./app [username] [execute program] [human vs human input dir] [human myOpic input dir]
+# EFFECT: generate a dir username_result, with test output inside.
+result=$1_result
+rm -r ${result}
+mkdir ${result}
+cd ${result}
+for file in $(ls ../$3); do
+    to_save="${file}_hh_res"
+    ../$2 h h <../$3/${file} >${to_save}
+done
+
+for file in $(ls ../$4); do
+    to_save="${file}_hm_res"
+    ../$2 h m ${file} <../$4/${file} >./${to_save}
+done
+
+int=-114514
+
+while (($int <= 114514)); do
+    to_save="${int}_mm_res"
+    ../$2 m m $int >${to_save}
+    add=500
+    int=$(expr $int + $add)
+done
---
+++ p4/sh_test/diff.sh
@@ -0,0 +1,15 @@
+#usage: ./diff.sh [use1] [user2]
+#REQUIRE: app.sh already done
+#OUTPUT: Nothing if one file is the same
+#        diff -y in detail in dir "diff_detail" if there is file difference
+rmdir diff_detail
+mkdir diff_detail
+for file in $(ls $1_result); do
+    result=$(diff $1_result/${file} $2_result/${file} -q)
+    if [ "$result" = "File $1_result/$file and $2_result/${file} differ" ]; then
+        echo $result
+        cd diff_detail
+        diff ../$1_result/${file} ../$2_result/${file} -y >${file}
+        cd ../
+    fi
+done
